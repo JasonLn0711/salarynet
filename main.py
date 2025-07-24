b4f4371df10d83d982bfc2921b6537d6da683ff8
Jason <cre062400@gmail.com>
2025-07-24 12:52:01 +0800

    Add salary after-tax calculator with basic assiumptions

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 # main.py
-print("Welcome to SalaryNet!")
+
+def calc_after_tax(salary):
+    # fixed ratio for tax calculation
+    labor_insurance = salary * 0.08
+    health_insurance = salary * 0.02
+    pension = salary * 0.06
+    income_tax = salary * 0.05
+
+    net_income = salary - (
+        labor_insurance + health_insurance + pension + income_tax)
+    return round(net_income, 2)
+
+if __name__ == "__main__":
+    salary = float(input("Enter your salary: "))
+    net = calc_after_tax(salary)
+    print(f"Your salary after tax is: {net}")
