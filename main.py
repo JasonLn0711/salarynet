356c0f4f90d4e0152921b769dfd02b53c9f560ec
Jason <cre062400@gmail.com>
2025-07-24 13:22:09 +0800

    feat: increase pension deduction to 8%

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def calc_after_tax(salary):
     labor_insurance = salary * 0.08
     health_insurance = salary * 0.02
     pension = salary * 0.10 # pension contribution changed to 10%
+    pension = salary * 0.08           # the initial one is 0.06
     income_tax = salary * 0.05
 
     net_income = salary - (
